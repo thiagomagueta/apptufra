1ca91049f9b39d9fab832926bbdd88f4a2962f33
thiagomagueta <prof.thiago.magueta@gmail.com>
2026-08-06 17:48:53 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
+
+const SUPABASE_KEY = "SUA_CHAVE_PUBLICA";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
